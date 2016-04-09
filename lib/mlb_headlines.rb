c0bdffed5a087caee131faa83256a607b37d850f
jbok4 <jbok4@aol.com>
2016-04-09 16:32:00 -0400

    finalize

--- lib/mlb_headlines.rb
+++ lib/mlb_headlines.rb
@@ -1,12 +1,11 @@
+require "Nokogiri"
+require "pry"
+require "open-uri"
+
 require 'mlb_headlines/headline'
 require 'mlb_headlines/scraper'
 require 'mlb_headlines/cli'
 require 'mlb_headlines/version'
-require_relative '../config/environment'
-
-require "Nokogiri"
-require "pry"
-require "open-uri"
 
 module MlbHeadlines
 end
